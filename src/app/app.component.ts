a594aabecc7dabcaaa9eaf38aed3c9cac24a2834
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-13 18:02:51 +0300

    creating form model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Registration';
+
+  // Form model
+  registrationForm = new FormGroup({ 
+    userName : new FormControl(''),
+    email : new FormControl(''),
+    subscribe : new FormControl(''),
+    password : new FormControl(''),
+    confirmPassword : new FormControl(''),
+    address : new FormGroup({ 
+      state : new FormControl(''),
+      city : new FormControl(''),
+      postalCode : new FormControl(''),
+    }),
+  });
+
 }
