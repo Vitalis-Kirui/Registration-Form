f03a9962abc6fd01c558ac95b9594f6dd79a4349
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-13 22:22:19 +0300

    updating custom validator to accept multiple parameters

--- src/app/validators/forbiddenWords.ts
+++ src/app/validators/forbiddenWords.ts
@@ -1,6 +1,18 @@
-import { AbstractControl } from "@angular/forms";
+import { AbstractControl, ValidatorFn } from "@angular/forms";
 
-export function forbiddenWordsValidator(control : AbstractControl) : {[key : string] : any} | null{
-    const forbiddenWord =/admin/.test(control.value);
-    return forbiddenWord? {'forbiddenWords' : {value : control.value} } : null;
+// basic custom validator
+
+// export function forbiddenWordsValidator(control : AbstractControl) : {[key : string] : any} | null{
+//     const forbiddenWord =/admin/.test(control.value);
+//     return forbiddenWord? {'forbiddenWords' : {value : control.value} } : null;
+// }
+
+// Modified to accept several parameters
+export function forbiddenWordsValidator(forbiddenTerm : RegExp) : ValidatorFn{
+
+    return (control : AbstractControl) : {[key : string] : any} | null => {
+        const forbiddenWord =forbiddenTerm.test(control.value);
+        return forbiddenWord? {'forbiddenWords' : {value : control.value} } : null;
+    };
+
 }
