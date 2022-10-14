2305e15420b2136b1c337faf6e601edae29a6633
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-14 14:57:34 +0300

    creating register function on service

--- src/app/Services/registration.service.ts
+++ src/app/Services/registration.service.ts
@@ -6,5 +6,12 @@ import {HttpClient} from '@angular/common/http';
 })
 export class RegistrationService {
 
+  url='http://localhost:3000/enroll';
+
   constructor(private http : HttpClient) { }
+
+  register(userData : any ){
+    return this.http.post<any>(this.url, userData);
+  }
+  
 }
